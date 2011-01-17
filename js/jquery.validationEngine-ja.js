fc1e36ae14e4dee368bc2edb41fa81afe00d273b
tomotomo <valueshare@gmail.com>
2011-01-18 02:02:15 +0900

    Create japanese language file.

---
+++ js/jquery.validationEngine-ja.js
@@ -0,0 +1,101 @@
+;/*****************************************************************
+ * Japanese language file for jquery.validationEngine.js
+ *
+ * Transrator: tomotomo ( Tomoyuki SUGITA )
+ * http://tomotomoSnippet.blogspot.com/
+ * Licenced under the MIT Licence
+ *******************************************************************/
+ /*
+ * Inline Form Validation Engine 1.7.3, jQuery plugin
+ * 
+ * Copyright(c) 2010, Cedric Dugas
+ * http://www.position-absolute.com
+ *	
+ * Form validation engine allowing custom regex rules to be added.
+ * Thanks to Francois Duquette and Teddy Limousin 
+ * and everyone helping me find bugs on the forum
+ * Licenced under the MIT Licence
+ */
+(function($) {
+	$.fn.validationEngineLanguage = function() {};
+	$.validationEngineLanguage = {
+		newLang: function() {
+			$.validationEngineLanguage.allRules = 	{"required":{    			// Add your regex rules here, you can take telephone as an example
+					"regex":"none",
+						"alertText":"* 必須項目です",
+						"alertTextCheckboxMultiple":"* 選択してください",
+						"alertTextCheckboxe":"* チェックボックスにチェックを入れてください"},
+					"length":{
+						"regex":"none",
+						"alertText":"* ",
+						"alertText2":"文字以上",
+						"alertText3": "文字以下で入力してください"},
+					"maxCheckbox":{
+						"regex":"none",
+						"alertText":"* チェックしすぎです"},	
+					"minCheckbox":{
+						"regex":"none",
+						"alertText":"* ",
+						"alertText2":"つ以上選択してください"},	
+					"equals":{
+						"regex":"none",
+						"alertText":"* 入力された値が一致しません"},		
+					"phone":{
+						// credit: jquery.h5validate.js / orefalo
+						"regex": /^([\+][0-9]{1,3}[ \.\-])?([\(]{1}[0-9]{2,6}[\)])?([0-9 \.\-\/]{3,20})((x|ext|extension)[ ]?[0-9]{1,4})?$/,
+						"alertText":"* 電話番号が正しくありません"},	
+					"email":{
+						// Shamelessly lifted from Scott Gonzalez via the Bassistance Validation plugin http://projects.scottsplayground.com/email_address_validation/
+						"regex": /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/,
+						"alertText":"* メールアドレスが正しくありません"},
+					"integer":{
+						"regex": /^[\-\+]?\d+$/,
+						"alertText":"* 半角数字で入力してください"},
+					"number":{
+						// Number, including positive, negative, and floating decimal. Credit: bassistance
+						"regex": /^[\-\+]?(?:\d+|\d{1,3}(?:,\d{3})+)(?:\.\d+)$/,
+						"alertText":"* Invalid floating decimal number"},
+					"date":{
+						// Date in ISO format. Credit: bassistance
+                         "regex":/^\d{4}[\/\-]\d{1,2}[\/\-]\d{1,2}$/,
+                         "alertText":"* 日付は YYYY-MM-DD の形式で入力してください"},
+                         
+                    "ipv4":{
+                    	"regex": /^([1-9][0-9]{0,2})+\.([1-9][0-9]{0,2})+\.([1-9][0-9]{0,2})+\.([1-9][0-9]{0,2})+$/,
+                    	"alertText":"* IPアドレスが正しくありません"},      
+                    "url":{
+                    	"regex":/^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/,
+                    	"alertText":"* URLが正しくありません"},
+					"onlyNumber":{
+						"regex":/^[0-9\ ]+$/,
+						"alertText":"* 半角数字のみ入力してください"},
+					"noSpecialCaracters":{
+						"regex":/^[0-9a-zA-Z]+$/,
+						"alertText":"* 半角英数字のみ入力してください"},
+					"ajaxUser":{
+						"file":"validateUser.php",
+						"extraData":"name=eric",
+						"alertTextOk":"* This user is available",	
+						"alertTextLoad":"* Loading, please wait",
+						"alertText":"* This user is already taken"},	
+					"ajaxName":{
+						"file":"validateUser.php",
+						"alertText":"* This name is already taken",
+						"alertTextOk":"* This name is available",	
+						"alertTextLoad":"* Loading, please wait"},		
+					"onlyLetter":{
+						"regex":/^[a-zA-Z\ \']+$/,
+						"alertText":"* 半角のアルファベット・スペース・'（アポストロフィ）のみで入力してください"},
+					// TODO : I cannot understand following.
+					"validate2fields":{
+    					"nname":"validate2fields",
+    					"alertText":"* You must have a firstname and a lastname"}	
+					};
+
+		}
+	};
+})(jQuery);
+
+$(document).ready(function() {	
+	$.validationEngineLanguage.newLang();
+});
