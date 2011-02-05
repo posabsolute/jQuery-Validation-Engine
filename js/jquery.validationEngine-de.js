ea7dfa0a4f0b4a28ee8c240ef1cfcbcf86163806
gomfunkel <leinich@gmx.net>
2011-02-05 15:30:58 +0100

    Fixed encoding issues

--- js/jquery.validationEngine-de.js
+++ js/jquery.validationEngine-de.js
@@ -8,13 +8,13 @@
                 "required": { // Add your regex rules here, you can take telephone as an example
                     "regex": "none",
                     "alertText": "* Dieses Feld ist ein Pflichtfeld",
-                    "alertTextCheckboxMultiple": "* Bitte wählen Sie eine Option",
+                    "alertTextCheckboxMultiple": "* Bitte w√§hlen Sie eine Option",
                     "alertTextCheckboxe": "* Dieses Feld ist ein Pflichtfeld"
                 },
                 "minSize": {
                     "regex": "none",
                     "alertText": "* Mindestens ",
-                    "alertText2": " Zeichen benötigt"
+                    "alertText2": " Zeichen ben√∂tigt"
                 },
                 "maxSize": {
                     "regex": "none",
@@ -39,48 +39,48 @@
                 },	
                 "maxCheckbox": {
                     "regex": "none",
-                    "alertText": "* Maximale Anzahl Markierungen überschritten"
+                    "alertText": "* Maximale Anzahl Markierungen √ºberschritten"
                 },
                 "minCheckbox": {
                     "regex": "none",
-                    "alertText": "* Bitte wählen Sie ",
+                    "alertText": "* Bitte w√§hlen Sie ",
                     "alertText2": " Optionen"
                 },
                 "equals": {
                     "regex": "none",
-                    "alertText": "* Felder stimmen nicht überein"
+                    "alertText": "* Felder stimmen nicht √ºberein"
                 },
                 "phone": {
                     // credit: jquery.h5validate.js / orefalo
                     "regex": /^([\+][0-9]{1,3}[ \.\-])?([\(]{1}[0-9]{2,6}[\)])?([0-9 \.\-\/]{3,20})((x|ext|extension)[ ]?[0-9]{1,4})?$/,
-                    "alertText": "* Ungültige Telefonnummer"
+                    "alertText": "* Ung√ºltige Telefonnummer"
                 },
                 "email": {
                     // Simplified, was not working in the Iphone browser
                     "regex": /^([A-Za-z0-9_\-\.\'])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,6})$/,
-                    "alertText": "* Ungültige E-Mail Adresse"
+                    "alertText": "* Ung√ºltige E-Mail Adresse"
                 },
                 "integer": {
                     "regex": /^[\-\+]?\d+$/,
-                    "alertText": "* Keine gültige Ganzzahl"
+                    "alertText": "* Keine g√ºltige Ganzzahl"
                 },
                 "number": {
                     // Number, including positive, negative, and floating decimal. credit: orefalo
                     "regex": /^[\-\+]?(([0-9]+)([\.,]([0-9]+))?|([\.,]([0-9]+))?)$/,
-                    "alertText": "* Keine gültige Fließkommazahl"
+                    "alertText": "* Keine g√ºltige Flie√ükommazahl"
                 },
                 "date": {
                     // Date in ISO format. Credit: bassistance
                     "regex": /^\d{4}[\/\-]\d{1,2}[\/\-]\d{1,2}$/,
-                    "alertText": "* Ungültiges Datumsformat, erwartet wird das Format JJJJ-MM-TT"
+                    "alertText": "* Ung√ºltiges Datumsformat, erwartet wird das Format JJJJ-MM-TT"
                 },
                 "ipv4": {
                     "regex": /^([1-9][0-9]{0,2})+\.([1-9][0-9]{0,2})+\.([1-9][0-9]{0,2})+\.([1-9][0-9]{0,2})+$/,
-                    "alertText": "* Ungültige IP Adresse"
+                    "alertText": "* Ung√ºltige IP Adresse"
                 },
                 "url": {
                     "regex": /^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/,
-                    "alertText": "* Ungültige URL"
+                    "alertText": "* Ung√ºltige URL"
                 },
                 "onlyNumberSp": {
                     "regex": /^[0-9\ ]+$/,
@@ -100,7 +100,7 @@
                     // you may want to pass extra data on the ajax call
                     "extraData": "name=eric",
                     "alertText": "* Dieser Benutzer ist bereits vergeben",
-                    "alertTextLoad": "* Überprüfe Angaben, bitte warten"
+                    "alertTextLoad": "* √úberpr√ºfe Angaben, bitte warten"
                 },
                 "ajaxNameCall": {
                     // remote json service location
@@ -108,9 +108,9 @@
                     // error
                     "alertText": "* Dieser Name ist bereits vergeben",
                     // if you provide an "alertTextOk", it will show as a green prompt when the field validates
-                    "alertTextOk": "* Dieser Name ist verfügbar",
+                    "alertTextOk": "* Dieser Name ist verf√ºgbar",
                     // speaks by itself
-                    "alertTextLoad": "* Überprüfe Angaben, bitte warten"
+                    "alertTextLoad": "* √úberpr√ºfe Angaben, bitte warten"
                 },
                 "validate2fields": {
                     "alertText": "* Bitte HELLO eingeben"
