2b329f3c7ee00c38f9b3fda6d4620ecff9964e54
mecik <mecik82@hotmail.com>
2011-04-01 16:19:48 +0800

    changed encoding to UTF8

--- js/languages/jquery.validationEngine-tr.js
+++ js/languages/jquery.validationEngine-tr.js
@@ -7,7 +7,7 @@
                 "required": { // Add your regex rules here, you can take telephone as an example
                     "regex": "none",
                     "alertText": "* Bu alan zorunludur",
-                    "alertTextCheckboxMultiple": "* Lütfen bir seçeneði iþaretleyiniz",
+                    "alertTextCheckboxMultiple": "* LÃ¼tfen bir seÃ§eneÄŸi iÅŸaretleyiniz",
                     "alertTextCheckboxe": "* Bu onay kutusu zorunludur"
                 },
                 "minSize": {
@@ -22,116 +22,116 @@
                 },
                 "min": {
                     "regex": "none",
-                    "alertText": "* Geçerli en küçük deðer: "
+                    "alertText": "* GeÃ§erli en kÃ¼Ã§Ã¼k deÄŸer: "
                 },
                 "max": {
                     "regex": "none",
-                    "alertText": "* Geçerli en yüksek deðer: "
+                    "alertText": "* GeÃ§erli en yÃ¼ksek deÄŸer: "
                 },
                 "past": {
                     "regex": "none",
-                    "alertText": "* Lütfen ",
+                    "alertText": "* LÃ¼tfen ",
                     "alertText2": " tarihinden daha ileri bir tarih giriniz "
                 },
                 "future": {
                     "regex": "none",
-                    "alertText": "* Lütfen ",
+                    "alertText": "* LÃ¼tfen ",
                     "alertText2": " tarihinden daha geri bir tarih giriniz "
 
                 },	
                 "maxCheckbox": {
                     "regex": "none",
-                    "alertText": "* Lütfen daha az onay kutusu iþareyleyiniz"
+                    "alertText": "* LÃ¼tfen daha az onay kutusu iÅŸareyleyiniz"
                 },
                 "minCheckbox": {
                     "regex": "none",
-                    "alertText": "* Lütfen en az ",
-                    "alertText2": " onay kutusunu iþaretleyiniz"
+                    "alertText": "* LÃ¼tfen en az ",
+                    "alertText2": " onay kutusunu iÅŸaretleyiniz"
                 },
                 "equals": {
                     "regex": "none",
-                    "alertText": "* Deðerler ayný olmalý"
+                    "alertText": "* DeÄŸerler aynÄ± olmalÄ±"
                 },
                 "phone": {
                     // credit: jquery.h5validate.js / orefalo
                     "regex": /^([\+][0-9]{1,3}[ \.\-])?([\(]{1}[0-9]{2,6}[\)])?([0-9 \.\-\/]{3,20})((x|ext|extension)[ ]?[0-9]{1,4})?$/,
-                    "alertText": "* Geçersiz telefon numarasý"
+                    "alertText": "* GeÃ§ersiz telefon numarasÄ±"
                 },
                 "email": {
                     // Simplified, was not working in the Iphone browser
                     "regex": /^([A-Za-z0-9_\-\.\'])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,6})$/,
-                    "alertText": "* Geçersiz eposta adresi"
+                    "alertText": "* GeÃ§ersiz eposta adresi"
                 },
                 "integer": {
                     "regex": /^[\-\+]?\d+$/,
-                    "alertText": "* Geçerli bir tam sayý deðil"
+                    "alertText": "* GeÃ§erli bir tam sayÄ± deÄŸil"
                 },
                 "number": {
                     // Number, including positive, negative, and floating decimal. credit: orefalo
                     "regex": /^[\-\+]?(([0-9]+)([\.,]([0-9]+))?|([\.,]([0-9]+))?)$/,
-                    "alertText": "* Geçerli bir noktalý sayý deðil"
+                    "alertText": "* GeÃ§erli bir noktalÄ± sayÄ± deÄŸil"
                 },
                 "date": {
                     "regex": /^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/,
-                    "alertText": "* Geçersiz tarih. Tarih YYYY-MM-DD formatýnda olmalý"
+                    "alertText": "* GeÃ§ersiz tarih. Tarih YYYY-MM-DD formatÄ±nda olmalÄ±"
                 },
                 "ipv4": {
                     "regex": /^((([01]?[0-9]{1,2})|(2[0-4][0-9])|(25[0-5]))[.]){3}(([0-1]?[0-9]{1,2})|(2[0-4][0-9])|(25[0-5]))$/,
-                    "alertText": "* Geçersiz IP adresi"
+                    "alertText": "* GeÃ§ersiz IP adresi"
                 },
                 "url": {
                     "regex": /^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/,
-                    "alertText": "* Geçersiz URL"
+                    "alertText": "* GeÃ§ersiz URL"
                 },
                 "onlyNumberSp": {
                     "regex": /^[0-9\ ]+$/,
-                    "alertText": "* Bu alanda sadece rakam olmalý"
+                    "alertText": "* Bu alanda sadece rakam olmalÄ±"
                 },
                 "onlyLetterSp": {
                     "regex": /^[a-zA-Z\ \']+$/,
-                    "alertText": "* Bu alanda sadece harf olmalý"
+                    "alertText": "* Bu alanda sadece harf olmalÄ±"
                 },
                 "onlyLetterNumber": {
                     "regex": /^[0-9a-zA-Z]+$/,
-                    "alertText": "* Bu alanda özel karakterler olamaz"
+                    "alertText": "* Bu alanda Ã¶zel karakterler olamaz"
                 },
                 // --- CUSTOM RULES -- Those are specific to the demos, they can be removed or changed to your likings
                 "ajaxUserCall": {
                     "url": "ajaxValidateFieldUser",
                     // you may want to pass extra data on the ajax call
                     "extraData": "name=eric",
-                    "alertText": "* Bu kullanýcý adý kullanýmda",
-                    "alertTextLoad": "* Doðrulanýyor, lütfen bekleyiniz"
+                    "alertText": "* Bu kullanÄ±cÄ± adÄ± kullanÄ±mda",
+                    "alertTextLoad": "* DoÄŸrulanÄ±yor, lÃ¼tfen bekleyiniz"
                 },
 				"ajaxUserCallPhp": {
                     "url": "phpajax/ajaxValidateFieldUser.php",
                     // you may want to pass extra data on the ajax call
                     "extraData": "name=eric",
                     // if you provide an "alertTextOk", it will show as a green prompt when the field validates
-                    "alertTextOk": "* Bu kullanýcý adýný kullanabilirsiniz",
-                    "alertText": "* Bu kullanýcý adý kullanýmda",
-                    "alertTextLoad": "* Doðrulanýyor, lütfen bekleyiniz"
+                    "alertTextOk": "* Bu kullanÄ±cÄ± adÄ±nÄ± kullanabilirsiniz",
+                    "alertText": "* Bu kullanÄ±cÄ± adÄ± kullanÄ±mda",
+                    "alertTextLoad": "* DoÄŸrulanÄ±yor, lÃ¼tfen bekleyiniz"
                 },
                 "ajaxNameCall": {
                     // remote json service location
                     "url": "ajaxValidateFieldName",
                     // error
-                    "alertText": "* Bu isim kullanýmda",
+                    "alertText": "* Bu isim kullanÄ±mda",
                     // if you provide an "alertTextOk", it will show as a green prompt when the field validates
-                    "alertTextOk": "* Bu isim kullanýlabilir",
+                    "alertTextOk": "* Bu isim kullanÄ±labilir",
                     // speaks by itself
-                    "alertTextLoad": "* Doðrulanýyor, lütfen bekleyiniz"
+                    "alertTextLoad": "* DoÄŸrulanÄ±yor, lÃ¼tfen bekleyiniz"
                 },
 				 "ajaxNameCallPhp": {
 	                    // remote json service location
 	                    "url": "phpajax/ajaxValidateFieldName.php",
 	                    // error
-	                    "alertText": "* Bu isim kullanýmda",
+	                    "alertText": "* Bu isim kullanÄ±mda",
 	                    // speaks by itself
-	                    "alertTextLoad": "* Doðrulanýyor, lütfen bekleyiniz"
+	                    "alertTextLoad": "* DoÄŸrulanÄ±yor, lÃ¼tfen bekleyiniz"
 	                },
                 "validate2fields": {
-                    "alertText": "* Lütfen 'HELLO' yazýn"
+                    "alertText": "* LÃ¼tfen 'HELLO' yazÄ±n"
                 }
             };
             
