47ed49c97192820ea42140a74c1ca96c6a6657ed
Olivier Refalo <orefalo@tracfone.com>
2012-02-06 14:51:22 -0500

    fix 219, passing only spaces to _required should raise validation error

--- js/jquery.validationEngine.js
+++ js/jquery.validationEngine.js
@@ -626,7 +626,7 @@
 				case "textarea":
 				case "file":
 				default:
-					if (!field.val())
+					if (!($.trim(field.val())))
 						return options.allrules[rules[i]].alertText;
 					break;
 				case "radio":
