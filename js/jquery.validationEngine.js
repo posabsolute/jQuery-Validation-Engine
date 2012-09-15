posabsolute/jQuery-Validation-Engine

commit 0bb6e7211570babae86df8f086c2593fc2ea76b3

fix #481

js/jquery.validationEngine.js
@@ -1,4 +1,4 @@
-/*
+2/*
  * Inline Form Validation Engine 2.6.1, jQuery plugin
  *
  * Copyright(c) 2010, Cedric Dugas
@@ -382,7 +382,7 @@
 
 					} else {
 						$("html, body").animate({
-							scrollTop: destination,
+							scrollTop: destination
 						}, 1100, function(){
 							if(options.focusFirstField) first_err.focus();
 						});

commit ac4d1b259dc1cbf17c470054d1fc80a7f426fb06

Fix for typo introduced in 0bb6e7211570babae86df8f086c2593fc2ea76b3.

js/jquery.validationEngine.js
@@ -1,4 +1,4 @@
-2/*
+/*
  * Inline Form Validation Engine 2.6.1, jQuery plugin
  *
  * Copyright(c) 2010, Cedric Dugas