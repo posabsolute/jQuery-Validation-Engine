391b738156e23e05121199d7539746bf8f2c0f68
Cedric Dugas <cedric.dugas@gmail.com>
2012-11-20 07:54:44 -0500

    fixed select multiple

--- js/jquery.validationEngine.js
+++ js/jquery.validationEngine.js
@@ -892,7 +892,7 @@
 				case "select-one":
 				case "select-multiple":
 				default:
-					if (! $.trim(field.val()) && ((field.val() != field.attr("data-validation-placeholder")) || (field.val() != field.attr("placeholder"))))
+					if (! $.trim(field.val()) && ((field.val() != field.attr("data-validation-placeholder")) || (field.val() !== field.attr("placeholder"))))
 						return options.allrules[rules[i]].alertText;
 					break;
 				case "radio":
