1689b1cfc5cb1bf90dae043d7fa71acefb9feae2
pl <pl71@server.fake>
2013-06-25 11:04:30 +0300

    language bg corrected

--- js/languages/jquery.validationEngine-bg.js
+++ js/languages/jquery.validationEngine-bg.js
@@ -8,7 +8,7 @@
                     "regex": "none",
                     "alertText": "* Това поле е задължително",
                     "alertTextCheckboxMultiple": "* Моля, изберете от списъка",
-                    "alertTextCheckboxe": "* Този чек е задължителен",
+                    "alertTextCheckboxe": "* Трябва да отметните",
                     "alertTextDateRange": "* И двете полета за дата са задължителни"
                 },
                 "requiredInFunction": { 
@@ -24,7 +24,7 @@
                 },
                 "dateTimeRange": {
                     "regex": "none",
-                    "alertText": "* Невалиден  				",
+                    "alertText": "* Невалиден ",
                     "alertText2": "дата/час период"
                 },
                 "minSize": {
@@ -37,7 +37,7 @@
                     "alertText": "* максимум ",
                     "alertText2": " символа"
                 },
-				"groupRequired": {
+                "groupRequired": {
                     "regex": "none",
                     "alertText": "* Трябва да попълните едно от следните полета"
                 },
@@ -95,25 +95,25 @@
                     "alertText": "* Невалидно десетично число"
                 },
                 "date": {                    
-                    //	Check if date is valid by leap year
-			"func": function (field) {
-					var pattern = new RegExp(/^(\d{4})[\/\-\.](0?[1-9]|1[012])[\/\-\.](0?[1-9]|[12][0-9]|3[01])$/);
-					var match = pattern.exec(field.val());
-					if (match == null)
-					   return false;
-	
-					var year = match[1];
-					var month = match[2]*1;
-					var day = match[3]*1;					
-					var date = new Date(year, month - 1, day); // because months starts from 0.
-	
-					return (date.getFullYear() == year && date.getMonth() == (month - 1) && date.getDate() == day);
-				},                		
-			 "alertText": "* Невалидна дата, трябва да бъде във формат: YYYY-MM-DD"
+                    //Check if date is valid by leap year
+                "func": function (field) {
+                    var pattern = new RegExp(/^(\d{4})[\/\-\.](0?[1-9]|1[012])[\/\-\.](0?[1-9]|[12][0-9]|3[01])$/);
+                    var match = pattern.exec(field.val());
+                    if (match === null)
+                       return false;
+                    
+                    var year = match[1];
+                    var month = match[2]*1;
+                    var day = match[3]*1;
+                    var date = new Date(year, month - 1, day); // because months starts from 0.
+                    
+                    return (date.getFullYear() == year && date.getMonth() == (month - 1) && date.getDate() == day);
+                    },
+                    "alertText": "* Невалидна дата, трябва да бъде във формат: YYYY-MM-DD"
                 },
                 "ipv4": {
                     "regex": /^((([01]?[0-9]{1,2})|(2[0-4][0-9])|(25[0-5]))[.]){3}(([0-1]?[0-9]{1,2})|(2[0-4][0-9])|(25[0-5]))$/,
-                    "alertText": "* Invalid IP address"
+                    "alertText": "* Невалиден IP адрес"
                 },
                 "url": {
                     "regex": /^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i,
@@ -121,12 +121,23 @@
                 },
                 "onlyNumberSp": {
                     "regex": /^[0-9\ ]+$/,
-                    "alertText": "* Само цифри"
+                    "alertText": "* Само цифри и интервал"
                 },
                 "onlyLetterSp": {
                     "regex": /^[a-zA-Z\ \']+$/,
                     "alertText": "* Само букви"
                 },
+                // --- Bulgarian NAMES in cyrillic alphabet
+                "onlyBGname": {
+                    "regex": /^[а-яА-Я\-]+$/,
+                    "alertText": "* Без латиница, цифри и спец. символи"
+                    },
+                // --- Only numbers, no space
+                "onlyDigits": {
+                    "regex": /^[0-9]+$/,
+                    "alertText": "* Само цифри"
+                },
+                
                 "onlyLetterNumber": {
                     "regex": /^[0-9a-zA-Z]+$/,
                     "alertText": "* Не са позволени спец. символи"
@@ -139,49 +150,49 @@
                     "alertText": "* Потребителското име е заето",
                     "alertTextLoad": "* Проверява се, моля почакайте"
                 },
-				"ajaxUserCallPhp": {
+                "ajaxUserCallPhp": {
                     "url": "phpajax/ajaxValidateFieldUser.php",
                     // you may want to pass extra data on the ajax call
                     "extraData": "name=eric",
                     // if you provide an "alertTextOk", it will show as a green prompt when the field validates
-                    "alertTextOk": "* This username is available",
-                    "alertText": "* This user is already taken",
-                    "alertTextLoad": "* Validating, please wait"
+                    "alertTextOk": "* Името е свободно",
+                    "alertText": "* Името е заето",
+                    "alertTextLoad": "* Проверява се, моля почакайте"
                 },
                 "ajaxNameCall": {
                     // remote json service location
                     "url": "ajaxValidateFieldName",
                     // error
-                    "alertText": "* This name is already taken",
+                    "alertText": "* Това име е заето",
                     // if you provide an "alertTextOk", it will show as a green prompt when the field validates
-                    "alertTextOk": "* This name is available",
+                    "alertTextOk": "* Това име е свободно",
                     // speaks by itself
-                    "alertTextLoad": "* Validating, please wait"
+                    "alertTextLoad": "* Проверява се, моля почакайте"
                 },
-				 "ajaxNameCallPhp": {
-	                    // remote json service location
-	                    "url": "phpajax/ajaxValidateFieldName.php",
-	                    // error
-	                    "alertText": "* This name is already taken",
-	                    // speaks by itself
-	                    "alertTextLoad": "* Validating, please wait"
-	                },
+                "ajaxNameCallPhp": {
+                        // remote json service location
+                        "url": "phpajax/ajaxValidateFieldName.php",
+                        // error
+                        "alertText": "* Името е заето",
+                        // speaks by itself
+                        "alertTextLoad": "* Проверява се, моля почакайте"
+                    },
                 "validate2fields": {
                     "alertText": "* Please input HELLO"
                 },
-	            //tls warning:homegrown not fielded 
+                //tls warning:homegrown not fielded 
                 "dateFormat":{
                     "regex": /^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$|^(?:(?:(?:0?[13578]|1[02])(\/|-)31)|(?:(?:0?[1,3-9]|1[0-2])(\/|-)(?:29|30)))(\/|-)(?:[1-9]\d\d\d|\d[1-9]\d\d|\d\d[1-9]\d|\d\d\d[1-9])$|^(?:(?:0?[1-9]|1[0-2])(\/|-)(?:0?[1-9]|1\d|2[0-8]))(\/|-)(?:[1-9]\d\d\d|\d[1-9]\d\d|\d\d[1-9]\d|\d\d\d[1-9])$|^(0?2(\/|-)29)(\/|-)(?:(?:0[48]00|[13579][26]00|[2468][048]00)|(?:\d\d)?(?:0[48]|[2468][048]|[13579][26]))$/,
                     "alertText": "* Невалидна дата"
                 },
                 //tls warning:homegrown not fielded 
-				"dateTimeFormat": {
-	                "regex": /^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])\s+(1[012]|0?[1-9]){1}:(0?[1-5]|[0-6][0-9]){1}:(0?[0-6]|[0-6][0-9]){1}\s+(am|pm|AM|PM){1}$|^(?:(?:(?:0?[13578]|1[02])(\/|-)31)|(?:(?:0?[1,3-9]|1[0-2])(\/|-)(?:29|30)))(\/|-)(?:[1-9]\d\d\d|\d[1-9]\d\d|\d\d[1-9]\d|\d\d\d[1-9])$|^((1[012]|0?[1-9]){1}\/(0?[1-9]|[12][0-9]|3[01]){1}\/\d{2,4}\s+(1[012]|0?[1-9]){1}:(0?[1-5]|[0-6][0-9]){1}:(0?[0-6]|[0-6][0-9]){1}\s+(am|pm|AM|PM){1})$/,
+                "dateTimeFormat": {
+                "regex": /^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])\s+(1[012]|0?[1-9]){1}:(0?[1-5]|[0-6][0-9]){1}:(0?[0-6]|[0-6][0-9]){1}\s+(am|pm|AM|PM){1}$|^(?:(?:(?:0?[13578]|1[02])(\/|-)31)|(?:(?:0?[1,3-9]|1[0-2])(\/|-)(?:29|30)))(\/|-)(?:[1-9]\d\d\d|\d[1-9]\d\d|\d\d[1-9]\d|\d\d\d[1-9])$|^((1[012]|0?[1-9]){1}\/(0?[1-9]|[12][0-9]|3[01]){1}\/\d{2,4}\s+(1[012]|0?[1-9]){1}:(0?[1-5]|[0-6][0-9]){1}:(0?[0-6]|[0-6][0-9]){1}\s+(am|pm|AM|PM){1})$/,
                     "alertText": "* Невалидна дата или грешен формат за дата",
                     "alertText2": "Очакван формат: ",
                     "alertText3": "mm/dd/yyyy hh:mm:ss AM|PM or ", 
                     "alertText4": "yyyy-mm-dd hh:mm:ss AM|PM"
-	            }
+                    }
             };
             
         }
