9c2b4a684f6bbbd064b0b69443f208e6a3b379a8
swashata <swashata4u@gmail.com>
2013-09-17 13:56:29 +0530

    Fixed IE8 Bug with indexOf
    Changed line 714 to use jQuery.inArray instead of indexOf to support
    IE8.
    
    Also, as it seems, my sublimetext got rid of trailing whitespaces.

--- js/jquery.validationEngine.js
+++ js/jquery.validationEngine.js
@@ -86,7 +86,7 @@
 			// unbind form.submit
 			form.off("submit", methods._onSubmitEvent);
 			form.removeData('jqv');
-            
+
 			form.off("click", "a[data-validation-engine-skip], a[class*='validate-skip'], button[data-validation-engine-skip], button[class*='validate-skip'], input[data-validation-engine-skip], input[class*='validate-skip']", methods._submitButtonClick);
 			form.removeData('jqv_submitButton');
 
@@ -110,7 +110,7 @@
 					// form is already validating.
 					// Should abort old validation and start new one. I don't know how to implement it.
 					return false;
-				} else {				
+				} else {
 					element.addClass('validating');
 					var options = element.data('jqv');
 					var valid = methods._validateFields(this);
@@ -200,7 +200,7 @@
 			 var options = form.data('jqv');
 			 var fadeDuration = (options && options.fadeDuration) ? options.fadeDuration : 0.3;
 			 var closingtag;
-			 
+
 			 if($(this).is("form") || $(this).hasClass("validationEngineContainer")) {
 				 closingtag = "parentForm"+methods._getClassName($(this).attr("id"));
 			 } else {
@@ -256,7 +256,7 @@
 		_onSubmitEvent: function() {
 			var form = $(this);
 			var options = form.data('jqv');
-			
+
 			//check if it is trigger from skipped button
 			if (form.data("jqv_submitButton")){
 				var submitButton = $("#" + form.data("jqv_submitButton"));
@@ -270,7 +270,7 @@
 
 			options.eventTrigger = "submit";
 
-			// validate each field 
+			// validate each field
 			// (- skip field ajax validation, not necessary IF we will perform an ajax form validation)
 			var r=methods._validateFields(form);
 
@@ -302,7 +302,7 @@
 			});
 			return status;
 		},
-		
+
 		/**
 		* Return true if the ajax field is validated
 		* @param {String} fieldid
@@ -384,7 +384,7 @@
 							destination=prompt_err.offset().top;
 						}
 					}
-					
+
 					// Offset the amount the page scrolls by an amount in px to accomodate fixed elements at top of page
 					if (options.scrollOffset) {
 						destination -= options.scrollOffset;
@@ -541,7 +541,7 @@
 			var limitErrors = false;
 			options.isError = false;
 			options.showArrow = true;
-			
+
 			// If the programmer wants to limit the amount of error messages per field,
 			if (options.maxErrorsPerField > 0) {
 				limitErrors = true;
@@ -558,7 +558,7 @@
 			}
 
 			for (var i = 0, field_errors = 0; i < rules.length; i++) {
-				
+
 				// If we are limiting errors, and have hit the max, break
 				if (limitErrors && field_errors >= options.maxErrorsPerField) {
 					// If we haven't hit a required yet, check to see if there is one in the validation rules for this
@@ -569,8 +569,8 @@
 					}
 					break;
 				}
-				
-				
+
+
 				var errorMsg = undefined;
 				switch (rules[i]) {
 
@@ -588,7 +588,7 @@
 						var firstOfGroup = form.find(classGroup).eq(0);
 						if(firstOfGroup[0] != field[0]){
 
-							methods._validateField(firstOfGroup, options, skipAjaxValidation); 
+							methods._validateField(firstOfGroup, options, skipAjaxValidation);
 							options.showArrow = true;
 
 						}
@@ -672,9 +672,9 @@
 
 					default:
 				}
-				
+
 				var end_validation = false;
-				
+
 				// If we were passed back an message object, check what the status was to determine what to do
 				if (typeof errorMsg == "object") {
 					switch (errorMsg.status) {
@@ -694,24 +694,24 @@
 							break;
 					}
 				}
-				
+
 				// If it has been specified that validation should end now, break
 				if (end_validation) {
 					break;
 				}
-				
+
 				// If we have a string, that means that we have an error, so add it to the error message.
 				if (typeof errorMsg == 'string') {
 					promptText += errorMsg + "<br/>";
 					options.isError = true;
 					field_errors++;
-				}	
+				}
 			}
 			// If the rules required is not added, an empty field is not validated
 			//the 3rd condition is added so that even empty password fields should be equal
 			//otherwise if one is filled and another left empty, the "equal" condition would fail
 			//which does not make any sense
-			if(!required && !(field.val()) && field.val().length < 1 && rules.indexOf("equals") < 0) options.isError = false;
+			if(!required && !(field.val()) && field.val().length < 1 && $.inArray('equals', rules) < 0) options.isError = false;
 
 			// Hack for radio/checkbox group button, the validation go into the
 			// first radio/checkbox of the group
@@ -749,9 +749,9 @@
 			} else if (!options.isError) {
 				options.InvalidFields.splice(errindex, 1);
 			}
-				
+
 			methods._handleStatusCssClasses(field, options);
-	
+
 			/* run callback function for each field */
 			if (options.isError && options.onFieldFailure)
 				options.onFieldFailure(field);
@@ -762,30 +762,30 @@
 			return options.isError;
 		},
 		/**
-		* Handling css classes of fields indicating result of validation 
+		* Handling css classes of fields indicating result of validation
 		*
 		* @param {jqObject}
 		*            field
 		* @param {Array[String]}
-		*            field's validation rules            
+		*            field's validation rules
 		* @private
 		*/
 		_handleStatusCssClasses: function(field, options) {
 			/* remove all classes */
 			if(options.addSuccessCssClassToField)
 				field.removeClass(options.addSuccessCssClassToField);
-			
+
 			if(options.addFailureCssClassToField)
 				field.removeClass(options.addFailureCssClassToField);
-			
+
 			/* Add classes */
 			if (options.addSuccessCssClassToField && !options.isError)
 				field.addClass(options.addSuccessCssClassToField);
-			
+
 			if (options.addFailureCssClassToField && options.isError)
-				field.addClass(options.addFailureCssClassToField);		
+				field.addClass(options.addFailureCssClassToField);
 		},
-		
+
 		 /********************
 		  * _getErrorMessage
 		  *
@@ -842,12 +842,12 @@
 			if (validityProp != undefined) {
 				custom_message = field.attr("data-errormessage-"+validityProp);
 				// If there was an error message for it, return the message
-				if (custom_message != undefined) 
+				if (custom_message != undefined)
 					return custom_message;
 			}
 			custom_message = field.attr("data-errormessage");
 			 // If there is an inline custom error message, return it
-			if (custom_message != undefined) 
+			if (custom_message != undefined)
 				return custom_message;
 			var id = '#' + field.attr("id");
 			// If we have custom messages for the element's id, get the message for the rule from the id.
@@ -963,7 +963,7 @@
 					isValid = true;
 					return false;
 				}
-			}); 
+			});
 
 			if(!isValid) {
 		  return options.allrules[rules[i]].alertText;
@@ -987,7 +987,7 @@
 				alert("jqv:custom rule not found - "+customRule);
 				return;
 			}
-			
+
 			if(rule["regex"]) {
 				 var ex=rule.regex;
 					if(!ex) {
@@ -997,15 +997,15 @@
 					var pattern = new RegExp(ex);
 
 					if (!pattern.test(field.val())) return options.allrules[customRule].alertText;
-					
+
 			} else if(rule["func"]) {
-				fn = rule["func"]; 
-				 
+				fn = rule["func"];
+
 				if (typeof(fn) !== "function") {
 					alert("jqv:custom parameter 'function' is no function - "+customRule);
 						return;
 				}
-				 
+
 				if (!fn(field, rules, i, options))
 					return options.allrules[customRule].alertText;
 			} else {
@@ -1391,7 +1391,7 @@
 					 }
 				 }
 			 }
-			 
+
 			 // If a field change event triggered this we want to clear the cache for this ID
 			 if (options.eventTrigger == "field") {
 				delete(options.ajaxValidCache[field.attr("id")]);
@@ -1469,7 +1469,7 @@
 									 else
 										methods._closePrompt(errorField);
 								}
-								
+
 								 // If a submit form triggered this, we want to re-submit the form
 								 if (options.eventTrigger == "submit")
 									field.closest("form").submit();
@@ -1478,7 +1478,7 @@
 						 errorField.trigger("jqv.field.result", [errorField, options.isError, msg]);
 					 }
 				 });
-				 
+
 				 return rule.alertTextLoad;
 			 }
 		 },
@@ -1540,7 +1540,7 @@
 			 // Because no error was found befor submitting
 			 if(ajaxform) prompt = false;
 			 // Check that there is indded text
-			 if($.trim(promptText)){ 
+			 if($.trim(promptText)){
 				 if (prompt)
 					methods._updatePrompt(field, prompt, promptText, type, ajaxed, options);
 				 else
@@ -1591,7 +1591,7 @@
 				var arrow = $('<div>').addClass("formErrorArrow");
 
 				//prompt positioning adjustment support. Usage: positionType:Xshift,Yshift (for ex.: bottomLeft:+20 or bottomLeft:-20,+10)
-				if (typeof(positionType)=='string') 
+				if (typeof(positionType)=='string')
 				{
 					var pos=positionType.indexOf(":");
 					if(pos!=-1)
@@ -1641,9 +1641,9 @@
 					field.after(prompt);
 				}
 			} else {
-				field.before(prompt);				
+				field.before(prompt);
 			}
-			
+
 			var pos = methods._calculatePosition(field, prompt, options);
 			prompt.css({
 				'position': positionType === 'inline' ? 'relative' : 'absolute',
@@ -1652,7 +1652,7 @@
 				"marginTop": pos.marginTopSize,
 				"opacity": 0
 			}).data("callerField", field);
-			
+
 
 			if (options.autoHidePrompt) {
 				setTimeout(function(){
@@ -1663,7 +1663,7 @@
 						prompt.remove();
 					});
 				}, options.autoHideDelay);
-			} 
+			}
 			return prompt.animate({
 				"opacity": 0.87
 			});
@@ -1781,9 +1781,9 @@
 
 			var promptTopPosition, promptleftPosition, marginTopSize;
 			var fieldWidth 	= field.width();
-			var fieldLeft 	= field.position().left; 
+			var fieldLeft 	= field.position().left;
 			var fieldTop 	=  field.position().top;
-			var fieldHeight 	=  field.height();	
+			var fieldHeight 	=  field.height();
 			var promptHeight = promptElmt.height();
 
 
@@ -1791,7 +1791,7 @@
 			promptTopPosition = promptleftPosition = 0;
 			// compensation for the arrow
 			marginTopSize = -promptHeight;
-		
+
 
 			//prompt positioning adjustment support
 			//now you can adjust prompt position
@@ -1828,7 +1828,7 @@
 				};
 			};
 
-			
+
 			switch (positionType) {
 				default:
 				case "topRight":
@@ -1838,7 +1838,7 @@
 
 				case "topLeft":
 					promptTopPosition +=  fieldTop;
-					promptleftPosition += fieldLeft; 
+					promptleftPosition += fieldLeft;
 					break;
 
 				case "centerRight":
@@ -1850,7 +1850,7 @@
 					promptleftPosition = fieldLeft - (promptElmt.width() + 2);
 					promptTopPosition = fieldTop+4;
 					marginTopSize = 0;
-					
+
 					break;
 
 				case "bottomLeft":
@@ -1869,7 +1869,7 @@
 					marginTopSize = 0;
 			};
 
-		
+
 
 			//apply adjusments if any
 			promptleftPosition += shiftX;
@@ -2038,7 +2038,7 @@
 		isError: false,
 		// Limit how many displayed errors a field can have
 		maxErrorsPerField: false,
-		
+
 		// Caches field validation status, typically only bad status are created.
 		// the array is used during ajax form validation to detect issues early and prevent an expensive submit
 		ajaxValidCache: {},
@@ -2053,7 +2053,7 @@
 		validateAttribute: "class",
 		addSuccessCssClassToField: "",
 		addFailureCssClassToField: "",
-		
+
 		// Auto-hide prompt
 		autoHidePrompt: false,
 		// Delay before auto-hide
