73cfa5824f4e399eef2745492652d613cfd8a2ec
cronzon <cronzon@switchfly.com>
2014-08-09 23:33:23 +0000

    Fixes bug #836

--- js/jquery.validationEngine.js
+++ js/jquery.validationEngine.js
@@ -1718,6 +1718,11 @@
 				"marginTop": pos.marginTopSize,
 				"opacity": 0.87};
 
+                prompt.css({
+                    "opacity": 0,
+                    "display": "block"
+                });
+                
 				if (noAnimation)
 					prompt.css(css);
 				else
