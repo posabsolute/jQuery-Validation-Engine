cfcd71a1597a0f4e83d5a38c45bd9cf459c7bc07
Emanuele Tessore <setola@gmail.com>
2014-10-15 11:34:38 +0200

    some fixes and extensions to the italian localization

--- js/languages/jquery.validationEngine-it.js
+++ js/languages/jquery.validationEngine-it.js
@@ -1,5 +1,6 @@
 (function($){
-    $.fn.validationEngineLanguage = function(){};
+    $.fn.validationEngineLanguage = function(){
+    };
     $.validationEngineLanguage = {
         newLang: function(){
             $.validationEngineLanguage.allRules = {
@@ -7,31 +8,65 @@
                     "regex": "none",
                     "alertText": "* Campo richiesto",
                     "alertTextCheckboxMultiple": "* Per favore selezionare un'opzione",
-                    "alertTextCheckboxe": "* E' richiesta la selezione della casella"
+                    "alertTextCheckboxe": "* E' richiesta la selezione della casella",
+                    "alertTextDateRange": "* Sono richiesti entrambi gli intervalli temporali"
                 },
-                "requiredInFunction": { 
+                "requiredInFunction": {
                     "func": function(field, rules, i, options){
                         return (field.val() == "test") ? true : false;
                     },
-                    "alertText": "* Field must equal test"
+                    "alertText": "* Il campo deve avere valore 'test'"
                 },
-                "length": {
+                "dateRange": {
                     "regex": "none",
-                    "alertText": "* Fra ",
-                    "alertText2": " e ",
-                    "alertText3": " caratteri permessi"
+                    "alertText": "* Intervallo ",
+                    "alertText2": "non valido"
+                },
+                "dateTimeRange": {
+                    "regex": "none",
+                    "alertText": "* Intervallo ",
+                    "alertText2": "non valido"
+                },
+                "minSize": {
+                    "regex": "none",
+                    "alertText": "* E' richiesto un minimo di ",
+                    "alertText2": " caratteri"
+                },
+                "maxSize": {
+                    "regex": "none",
+                    "alertText": "* E' richiesto un massimo di ",
+                    "alertText2": " caratteri"
+                },
+                "groupRequired": {
+                    "regex": "none",
+                    "alertText": "* Uno dei seguenti campi deve essere selezionato",
+                    "alertTextCheckboxMultiple": "* Selezionare una opzione",
+                    "alertTextCheckboxe": "* Segno di spunta richiesto"
+                },
+                "min": {
+                    "regex": "none",
+                    "alertText": "* Il valore minimo accettato è "
+                },
+                "max": {
+                    "regex": "none",
+                    "alertText": "* Il valore massimo accettato è "
+                },
+                "past": {
+                    "regex": "none",
+                    "alertText": "* Data antecedente al "
+                },
+                "future": {
+                    "regex": "none",
+                    "alertText": "* Data successiva al "
                 },
                 "maxCheckbox": {
                     "regex": "none",
-                    "alertText": "* Numero di caselle da selezionare in eccesso"
-                },
-				"groupRequired": {
-                    "regex": "none",
-                    "alertText": "* You must fill one of the following fields"
+                    "alertText": "* Massimo ",
+                    "alertText2": " opzioni consentite"
                 },
                 "minCheckbox": {
                     "regex": "none",
-                    "alertText": "* Per favore selezionare ",
+                    "alertText": "* Selezionare almeno ",
                     "alertText2": " opzioni"
                 },
                 "equals": {
@@ -40,7 +75,7 @@
                 },
                 "creditCard": {
                     "regex": "none",
-                    "alertText": "* Non valido numero di carta di credito"
+                    "alertText": "* Numero di carta di credito non valido"
                 },
                 "phone": {
                     // credit: jquery.h5validate.js / orefalo
@@ -48,68 +83,124 @@
                     "alertText": "* Numero di telefono non corretto"
                 },
                 "email": {
-                    // Shamelessly lifted from Scott Gonzalez via the Bassistance Validation plugin http://projects.scottsplayground.com/email_address_validation/
-                    "regex": /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i,
+                    // HTML5 compatible email regex ( http://www.whatwg.org/specs/web-apps/current-work/multipage/states-of-the-type-attribute.html#    e-mail-state-%28type=email%29 )
+                    "regex": /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
                     "alertText": "* Indirizzo non corretto"
+                },
+                "fullname": {
+                    "regex":/^([a-zA-Z]+[\'\,\.\-]?[a-zA-Z ]*)+[ ]([a-zA-Z]+[\'\,\.\-]?[a-zA-Z ]+)+$/,
+                    "alertText":"* Nome e cognome richiesti"
+                },
+                "zip": {
+                    "regex":/^\d{5}$|^\d{5}-\d{4}$/,
+                    "alertText":"* Formato zip non valido"
                 },
                 "integer": {
                     "regex": /^[\-\+]?\d+$/,
-                    "alertText": "* Numero intero non corretto"
+                    "alertText": "* Richiesto un numero intero"
                 },
                 "number": {
-                    // Number, including positive, negative, and floating decimal. Credit: bassistance
-                    "regex": /^[\-\+]?(?:\d+|\d{1,3}(?:,\d{3})+)(?:\.\d+)$/,
-                    "alertText": "* Numero decimale non corretto"
+                    // Number, including positive, negative, and floating decimal. credit: orefalo
+                    "regex": /^[\-\+]?((([0-9]{1,3})([,][0-9]{3})*)|([0-9]+))?([\.]([0-9]+))?$/,
+                    "alertText": "* Richiesto un numero decimale"
                 },
                 "date": {
-                    "regex": /^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/,
-                    "alertText": "* Data non  corretta, re-inserire secondo formato AAAA-MM-GG"
+                    //	Check if date is valid by leap year
+                    "func": function (field) {
+                        var pattern = new RegExp(/^(\d{4})[\/\-\.](0?[1-9]|1[012])[\/\-\.](0?[1-9]|[12][0-9]|3[01])$/);
+                        var match = pattern.exec(field.val());
+                        if (match == null)
+                            return false;
+
+                        var year = match[1];
+                        var month = match[2]*1;
+                        var day = match[3]*1;
+                        var date = new Date(year, month - 1, day); // because months starts from 0.
+
+                        return (date.getFullYear() == year && date.getMonth() == (month - 1) && date.getDate() == day);
+                    },
+                    "alertText": "* Data non corretta, è richeisto il formato AAAA-MM-GG"
                 },
-                
                 "ipv4": {
-                	"regex": /^((([01]?[0-9]{1,2})|(2[0-4][0-9])|(25[0-5]))[.]){3}(([0-1]?[0-9]{1,2})|(2[0-4][0-9])|(25[0-5]))$/,
+                    "regex": /^((([01]?[0-9]{1,2})|(2[0-4][0-9])|(25[0-5]))[.]){3}(([0-1]?[0-9]{1,2})|(2[0-4][0-9])|(25[0-5]))$/,
                     "alertText": "* IP non corretto"
                 },
                 "url": {
                     "regex": /^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i,
                     "alertText": "* URL non corretta"
                 },
-                "onlyNumber": {
+                "onlyNumberSp": {
                     "regex": /^[0-9\ ]+$/,
                     "alertText": "* Solo numeri"
                 },
-				"onlyLetter": {
+                "onlyLetterSp": {
                     "regex": /^[a-zA-Z\ \']+$/,
                     "alertText": "* Solo lettere"
                 },
-				"onlyLetterAccentSp":{
+                "onlyLetterAccentSp":{
                     "regex": /^[a-z\u00C0-\u017F\ ]+$/i,
-                    "alertText": "* Solo lettere"
+                    "alertText": "* Solo lettere (è possibile inserire lettere accentate)"
                 },
-                "validate2fields": {
-                    "nname": "validate2fields",
-                    "alertText": "* Occorre inserire nome e cognome"
-                },
-                "noSpecialCharacters": {
+                "onlyLetterNumber": {
                     "regex": /^[0-9a-zA-Z]+$/,
-                    "alertText": "* Caratteri speciali non permessi"
+                    "alertText": "* Non è possibile inserire caratteri speciali"
                 },
+                // --- CUSTOM RULES -- Those are specific to the demos, they can be removed or changed to your likings
                 "ajaxUserCall": {
-                    "file": "ajaxValidateFieldName",
+                    "url": "ajaxValidateFieldUser",
+                    // you may want to pass extra data on the ajax call
                     "extraData": "name=eric",
-                    "alertTextLoad": "* Caricamento, attendere per favore",
-                    "alertText": "* Questo user � gi� stato utilizzato"
+                    "alertText": "* Questo nome utente è già stato registrato",
+                    "alertTextLoad": "* Caricamento in corso, attendere prego"
+                },
+                "ajaxUserCallPhp": {
+                    "url": "phpajax/ajaxValidateFieldUser.php",
+                    // you may want to pass extra data on the ajax call
+                    "extraData": "name=eric",
+                    // if you provide an "alertTextOk", it will show as a green prompt when the field validates
+                    "alertTextOk": "* Questo nome utente è disponibile",
+                    "alertText": "* Questo nome utente è già stato registrato",
+                    "alertTextLoad": "* Caricamento in corso, attendere prego"
                 },
                 "ajaxNameCall": {
-                    "file": "ajaxValidateFieldName",
-                    "alertText": "* Questo nome � gi� stato utilizzato",
-                    "alertTextOk": "* Questo nome � disponibile",
-                    "alertTextLoad": "* Caricamento, attendere per favore"
+                    // remote json service location
+                    "url": "ajaxValidateFieldName",
+                    // error
+                    "alertText": "* Questo nome utente è già stato registrato",
+                    // if you provide an "alertTextOk", it will show as a green prompt when the field validates
+                    "alertTextOk": "* Questo nome utente è disponibile",
+                    // speaks by itself
+                    "alertTextLoad": "* Caricamento in corso, attendere prego"
+                },
+                "ajaxNameCallPhp": {
+                    // remote json service location
+                    "url": "phpajax/ajaxValidateFieldName.php",
+                    // error
+                    "alertText": "* Questo nome utente è già stato registrato",
+                    // speaks by itself
+                    "alertTextLoad": "* Caricamento in corso, attendere prego"
+                },
+                "validate2fields": {
+                    "alertText": "* Prego inserire 'HELLO'"
+                },
+                //tls warning:homegrown not fielded
+                "dateFormat":{
+                    "regex": /^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$|^(?:(?:(?:0?[13578]|1[02])(\/|-)31)|(?:(?:0?[1,3-9]|1[0-2])(\/|-)(?:29|30)))(\/|-)(?:[1-9]\d\d\d|\d[1-9]\d\d|\d\d[1-9]\d|\d\d\d[1-9])$|^(?:(?:0?[1-9]|1[0-2])(\/|-)(?:0?[1-9]|1\d|2[0-8]))(\/|-)(?:[1-9]\d\d\d|\d[1-9]\d\d|\d\d[1-9]\d|\d\d\d[1-9])$|^(0?2(\/|-)29)(\/|-)(?:(?:0[48]00|[13579][26]00|[2468][048]00)|(?:\d\d)?(?:0[48]|[2468][048]|[13579][26]))$/,
+                    "alertText": "* Data non valida"
+                },
+                //tls warning:homegrown not fielded
+                "dateTimeFormat": {
+                    "regex": /^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])\s+(1[012]|0?[1-9]){1}:(0?[1-5]|[0-6][0-9]){1}:(0?[0-6]|[0-6][0-9]){1}\s+(am|pm|AM|PM){1}$|^(?:(?:(?:0?[13578]|1[02])(\/|-)31)|(?:(?:0?[1,3-9]|1[0-2])(\/|-)(?:29|30)))(\/|-)(?:[1-9]\d\d\d|\d[1-9]\d\d|\d\d[1-9]\d|\d\d\d[1-9])$|^((1[012]|0?[1-9]){1}\/(0?[1-9]|[12][0-9]|3[01]){1}\/\d{2,4}\s+(1[012]|0?[1-9]){1}:(0?[1-5]|[0-6][0-9]){1}:(0?[0-6]|[0-6][0-9]){1}\s+(am|pm|AM|PM){1})$/,
+                    "alertText": "* Data o formato non validi",
+                    "alertText2": "Formato richiesto: ",
+                    "alertText3": "mm/gg/aaaa oo:mm:ss AM|PM oppure ",
+                    "alertText4": "aaaa-mm-gg oo:mm:ss AM|PM"
                 }
-
             };
-            
+
         }
     };
+
     $.validationEngineLanguage.newLang();
+
 })(jQuery);
